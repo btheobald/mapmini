3b4c0aace11389a7e3697f0038c5ee0b8ecadbd7
Byron Theobald <byrontheobald@gmail.com>
2021-11-08 17:11:47 +0000

    added to rendering test in agg_hal

--- lib/agg_sdl2/agg_hal.cpp
+++ lib/agg_sdl2/agg_hal.cpp
@@ -6,9 +6,13 @@
 #include "agg_pixfmt_rgba.h"
 #include "agg_renderer_base.h"
 #include "agg_conv_stroke.h"
+#include "agg_conv_curve.h"
+#include "agg_conv_dash.h"
 #include "agg_rasterizer_scanline_aa.h"
 #include "agg_renderer_scanline.h"
 #include "agg_scanline_p.h"
+#include "agg_ellipse.h"
+#include "agg_rounded_rect.h"
 #include "path.h"
 
 static SDL_Window *window = NULL;
@@ -85,10 +89,37 @@ void agg_hal_close(void)
     SDL_Quit();
 }
 
+ void drawLine (agg::renderer_base<agg::pixfmt_rgba32>& rBase, double x1, double y1, double x2, double y2, double lineWidth, agg::rgba8 color) {
+     agg::rasterizer_scanline_aa<> ras;
+     agg::scanline_p8 scanline;
+ 
+     ras.auto_close(false);
+ 
+     double linePoints[] = { x1, y1, x2, y2 };
+     SimplePath path (linePoints, sizeof(linePoints) / sizeof(double));
+     agg::conv_stroke<SimplePath> strokePath (path);
+ 
+     strokePath.width(lineWidth);
+     strokePath.line_cap(agg::butt_cap);
+     strokePath.line_join(agg::miter_join);
+     strokePath.miter_limit(lineWidth);
+ 
+     ras.reset();
+     ras.add_path(strokePath);
+ 
+     agg::render_scanlines_aa_solid(ras, scanline, rBase, color);
+ }
+
 void agg_hal_test(void) {
      const agg::rgba8 transparentWhiteColor (0xff, 0xff, 0xff, 0);
-     const agg::rgba8 greenColor (0, 0xff, 0, 0xff);
      const agg::rgba8 redColor (0xff, 0, 0, 0xff);
+     const agg::rgba8 pinkColor (0xff, 0, 0xff, 0xff);
+     const agg::rgba8 greenColor (0, 0xff, 0, 0xff);
+     const agg::rgba8 cyanColor (0, 0xff, 0xff, 0xff);
+     const agg::rgba8 blueColor (0, 0, 0xff, 0xff);
+     const agg::rgba8 yellowColor (0xff, 0xff, 0x0, 0xff);
+     const agg::rgba8 blackColor (0, 0, 0, 0xff);
+     const agg::rgba8 opaqueRedColor (0xff, 0x00, 0x00, 0x44);
 
      // clear the buffer with transparent white color
      rBase.clear(transparentWhiteColor);
@@ -150,6 +181,115 @@ void agg_hal_test(void) {
          ras.add_path(strokePath);
 
          agg::render_scanlines_aa_solid(ras, scanline, rBase, redColor);
+     }
+     {
+       // draw a square with a square hole
+       CmdVertex squares[] =
+       {
+          // outer square is CW
+          { agg::path_cmd_move_to, 100, 110 },
+          { agg::path_cmd_line_to, 160, 110 },
+          { agg::path_cmd_line_to, 160, 190 },
+          { agg::path_cmd_line_to, 100, 190 },
+          { agg::path_cmd_line_to, 100, 110 },
+
+          // inner square is also CW
+          { agg::path_cmd_move_to, 100, 130 },
+          { agg::path_cmd_line_to, 140, 130 },
+          { agg::path_cmd_line_to, 140, 170 },
+          { agg::path_cmd_line_to, 100, 170 },
+          { agg::path_cmd_line_to, 100, 130 },
+       };
+
+       drawLine (rBase, 0, 5, 100, 5, 0.5, blackColor);
+       drawLine (rBase, 0, 8.1, 100, 8.1, 0.5, blackColor);
+       drawLine (rBase, 0, 11.3, 100, 11.3, 0.5, blackColor);
+       drawLine (rBase, 0, 14.5, 100, 14.5, 0.5, blackColor);
+       drawLine (rBase, 0, 17.7, 100, 17.7, 0.5, blackColor);
+     
+
+         
+         CmdVertex tmp = squares[6];
+         squares[6] = squares[8];
+         squares[8] = tmp;
+
+         CmdVertexPath path (squares, sizeof(squares) / sizeof(CmdVertex));
+
+       ras.reset();
+       ras.add_path(path);
+
+       agg::render_scanlines_aa_solid(ras, scanline, rBase, blueColor);
+
+     }
+     {
+
+        CmdVertex dome[] =
+         {
+            { agg::path_cmd_move_to,  20, 200 },
+            { agg::path_cmd_curve3,  150, 300 },
+            { agg::path_cmd_curve3,  200, 100 },
+         };
+ 
+         CmdVertexPath path (dome, sizeof(dome) / sizeof(CmdVertex));
+ 
+         // draw a dashed dot caret
+         {
+             double strokeWidth = 5.0;
+ 
+             agg::conv_dash<CmdVertexPath> dashPath (path);
+ 
+             // a long dash
+             dashPath.add_dash(3 * strokeWidth, 3 * strokeWidth);
+             // a dot
+             dashPath.add_dash(1 * strokeWidth, 3 * strokeWidth);
+ 
+             agg::conv_stroke<agg::conv_dash<CmdVertexPath> > strokePath (dashPath);
+ 
+             strokePath.width(strokeWidth);
+             strokePath.line_cap(agg::butt_cap);
+             strokePath.line_join(agg::miter_join);
+             strokePath.miter_limit(strokeWidth);
+ 
+             ras.reset();
+             ras.add_path(strokePath);
+ 
+             agg::render_scanlines_aa_solid(ras, scanline, rBase, yellowColor);
+         }
+ 
+         // draw a dashed dot dome
+         {
+             double strokeWidth = 5.0;
+ 
+             // generate a curved path
+             agg::conv_curve<CmdVertexPath> curvePath (path);
+ 
+             // and then use this curved path to generate the stroke path
+             agg::conv_stroke<agg::conv_curve<CmdVertexPath> > strokePath (curvePath);
+ 
+             strokePath.width(strokeWidth);
+             strokePath.line_cap(agg::square_cap);
+             strokePath.line_join(agg::miter_join);
+             strokePath.miter_limit(strokeWidth);
+ 
+             ras.reset();
+             ras.add_path(strokePath);
+ 
+             agg::render_scanlines_aa_solid(ras, scanline, rBase, pinkColor);
+         }
+         
+         {
+             agg::ellipse eliipse (200, 70, 20, 50);
+             agg::rounded_rect rect2 (10, 100, 110, 320, 10);
+             
+             ras.reset();
+             ras.add_path(rect2);
+             agg::render_scanlines_aa_solid(ras, scanline, rBase, opaqueRedColor);
+ 
+             ras.reset();
+             ras.add_path(eliipse);
+ 
+             agg::render_scanlines_aa_solid(ras, scanline, rBase, blackColor);
+         }
      }
 
      agg_hal_flush();
